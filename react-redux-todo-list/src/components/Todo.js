c5d0c58553d321a2345ae56f1b02719c134d8dfe
Joshua Reed <joshualreed93@gmail.com>
2020-06-09 18:28:53 -0400

    index reducer & Todo component

---
+++ react-redux-todo-list/src/components/Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Todo = ({onCLick, completed, text}) => {
+    return (
+        <li onClick={onCLick}
+            style={{
+                textDecoration: completed ? 'line-through : 'none
+            }}>
+            {text}
+        </li>
+    )
+}
+
+Todo.propTypes = {
+onCLick: PropTypes.func.isRequired,
+completed: PropTypes.bool.isRequired,
+text: PropTypes.string.isRequired,
+}
+
+export default Todo
