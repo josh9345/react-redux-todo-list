bdd38c3590be952394d5bfcb65da4bcd6f6bf126
Joshua Reed <joshualreed93@gmail.com>
2020-06-10 08:45:49 -0400

    Todo component

--- react-redux-todo-list/src/components/Todo.js
+++ react-redux-todo-list/src/components/Todo.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Todo = ({onCLick, completed, text}) => {
-    return (
-        <li onClick={onCLick}
-            style={{
-                textDecoration: completed ? 'line-through : 'none
-            }}>
-            {text}
-        </li>
-    )
-}
+const Todo = ({ onCLick, completed, text }) => {
+  return (
+    <li
+      onClick={onCLick}
+      style={{
+        textDecoration: completed ? "line-through" : none,
+      }}
+    >
+      {text}
+    </li>
+  );
+};
 
 Todo.propTypes = {
-onCLick: PropTypes.func.isRequired,
-completed: PropTypes.bool.isRequired,
-text: PropTypes.string.isRequired,
-}
+  onCLick: PropTypes.func.isRequired,
+  completed: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
-export default Todo
+export default Todo;
